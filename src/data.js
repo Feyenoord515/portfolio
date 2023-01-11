0cf4e8ef0d7de86eb3f7fe3f7d44334e3a93da78
Feyenoord515 <n.enriquemolinari@gmail.com>
2023-01-11 19:31:38 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,69 @@
+export const projects = [
+  {
+    title: "Countries app",
+    subtitle: "PERN Stack",
+    description:
+      "The general idea was to create an application in which you can see information from different countries, using the external restcountries api. From it you can, among other things: search for countries, filter/order them and create tourist activities. For the filtering and sorting functionalities we did not use the external API endpoints that already return the filtered or ordered results but we had to do it ourselves. In particular, some of the ordering or filtering should be done from the frontend.",
+    image: "./countries.png", 
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Winery project",
+    subtitle: "PERN Stack",
+    description:
+    "The project consisted of comprehensively planning a complete e-commerce, with a database, backend and frontend. In this case it was the Winery project, with access for users and administrators. With stock control, sales, payment gateways, opinions on the different products, access with username and password for registered users, user registration form, location with maps of the different withdrawal points",
+    image: "./wines.jpg",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Recipes search",
+    subtitle: "PERN Stack",
+    description:
+      "The idea here was to solve what I cook with what I have at home. A search engine for recipes by ingredients. By clicking on the recipe, you can access the complete ingredients and the step by step. Database with recipes, backend and frontend. The styles are minimalist because of the context in which the app will be used.",
+    image: "./recipes.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Weather app",
+    subtitle: "React Hooks",
+    description:
+      "Information about the weather with city searches and weather in the current city. CSS and Bootstrap for the styles",
+    image: "./Clima.png",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+     "The secret of moving forward is to start",
+    image: "https://cdn.pixabay.com/photo/2016/08/18/05/05/mark-twain-1602117_1280.jpg",
+    name: "Mark Twain",
+    company: "Writer. Tireless adventurer",
+  },
+  {
+    quote:
+     "The only way to do a great job is to love what you do...",
+    image: "https://cdn.pixabay.com/photo/2018/12/19/14/50/steve-jobs-3884205__340.png",
+    name: "Steve Jobs",
+    company: "Co-founder and CEO of Apple",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "PostgreSQL",
+  "Sequelize",
+  "Express",
+  "Material UI",
+   "HTML",
+   "CSS",
+"SCRUM methodologies",
+  "Tailwind",
+  "Git",
+   "Github",
+   "Bootstrap"
+];
